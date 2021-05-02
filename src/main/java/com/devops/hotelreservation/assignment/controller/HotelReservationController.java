a3058c709a5b78fecb0685c896a1dda4c8072944
RAVIGANESHMBHAT <raviganeshmbhat999@gmail.com>
2021-05-02 17:05:40 +0530

    Bootstrapping added to homepage

--- src/main/java/com/devops/hotelreservation/assignment/controller/HotelReservationController.java
+++ src/main/java/com/devops/hotelreservation/assignment/controller/HotelReservationController.java
@@ -1,13 +1,13 @@
 package com.devops.hotelreservation.assignment.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HotelReservationController {
 
     @GetMapping("/")
     public String homePage() {
-        return "Home page.";
+        return "home-page";
     }
 }
