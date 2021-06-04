56a25f39bac0ca8b25bca4fe34094712445a926f
Shweta <shwetagln66@gmail.com>
2021-06-04 22:09:39 +0530

    JUnits

--- src/test/java/com/devops/hotelreservation/assignment/AssignmentApplicationTests.java
+++ src/test/java/com/devops/hotelreservation/assignment/AssignmentApplicationTests.java
@@ -1,13 +1,29 @@
 package com.devops.hotelreservation.assignment;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-class AssignmentApplicationTests {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-	@Test
-	void contextLoads() {
-	}
+// @RunWith attaches a runner with the test class to initialize the test data
+@RunWith(MockitoJUnitRunner.class)
+public class MathApplicationTester {
 
+  // @InjectMocks annotation is used to create and inject the mock object
+  @InjectMocks
+  UserServiceImpl userServiceImpl = new UserServiceImpl();
+
+  // @Mock annotation is used to create the mock object to be injected
+  @Mock
+  UserService userService;
+
+  @Test
+   public void testAdd(){
+      //add the behavior of calc service to add two numbers
+      when(UserService.findUserByEmail(shwetagln66@gmail.com)).thenReturn(null);
+   }
 }
