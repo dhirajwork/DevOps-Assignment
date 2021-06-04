2a7b5771fdc91e0fd6f0b0c94865d16a4379eff7
Shweta <shwetagln66@gmail.com>
2021-06-04 23:12:54 +0530

    added unit test

--- src/test/java/com/devops/hotelreservation/assignment/AssignmentApplicationTests.java
+++ src/test/java/com/devops/hotelreservation/assignment/AssignmentApplicationTests.java
@@ -1,29 +1,38 @@
 package com.devops.hotelreservation.assignment;
-
-import static org.mockito.Mockito.when;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-
-// @RunWith attaches a runner with the test class to initialize the test data
-@RunWith(MockitoJUnitRunner.class)
-public class AssignmentApplicationTests {
-
-  // @InjectMocks annotation is used to create and inject the mock object
-  @InjectMocks
-  UserServiceImpl userServiceImpl = new UserServiceImpl();
-
-  // @Mock annotation is used to create the mock object to be injected
-  @Mock
-  UserService userService;
-
-  @Test
-   public void testAdd(){
-      //add the behavior of calc service to add two numbers
-      when(UserService.findUserByEmail(shwetagln66@gmail.com)).thenReturn(null);
+import com.devops.hotelreservation.assignment.validation.EmailValidator;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import javax.validation.ClockProvider;
+import javax.validation.ConstraintValidatorContext;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@SpringBootTest
+class AssignmentApplicationTests {
+   @Test
+   void contextLoads() {
+   }
+   @Test
+   void testValidEmail(){
+      EmailValidator ev = new EmailValidator();
+      assertEquals(ev.isValid("abcxyz@test.com", new ConstraintValidatorContext() {
+         @Override
+         public void disableDefaultConstraintViolation() {
+         }
+         @Override
+         public String getDefaultConstraintMessageTemplate() {
+            return null;
+         }
+         @Override
+         public ClockProvider getClockProvider() {
+            return null;
+         }
+         @Override
+         public ConstraintViolationBuilder buildConstraintViolationWithTemplate(String messageTemplate) {
+            return null;
+         }
+         @Override
+         public <T> T unwrap(Class<T> type) {
+            return null;
+         }
+      }),true);
    }
 }
